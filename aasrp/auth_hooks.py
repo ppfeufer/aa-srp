01b58b0026cae9b3ad61e4483ed6d3f9f259f646
Peter Pfeufer <info@ppfeufer.de>
2020-11-06 13:42:19 +0100

    pending counter on menu item added

--- aasrp/auth_hooks.py
+++ aasrp/auth_hooks.py
@@ -4,25 +4,28 @@
 hook into AA
 """
 
-from django.utils.translation import ugettext_lazy as _
-
 from aasrp import urls, __title__
+from aasrp.managers import AaSrpManager
+
+from django.utils.translation import ugettext_lazy as _
 
 from allianceauth.services.hooks import MenuItemHook, UrlHook
 from allianceauth import hooks
 
 
 class AaSrpMenuItem(MenuItemHook):  # pylint: disable=too-few-public-methods
-    """ This class ensures only authorized users will see the menu entry """
+    """
+    This class ensures only authorized users will see the menu entry
+    """
 
     def __init__(self):
         # setup menu entry for sidebar
         MenuItemHook.__init__(
             self,
             _(__title__),
-            "fas fa-signal fa-fw",
+            "far fa-money-bill-alt fa-fw",
             "aasrp:dashboard",
-            navactive=["aasrp:dashboard"],
+            navactive=["aasrp:"],
         )
 
     def render(self, request):
@@ -31,7 +34,11 @@ class AaSrpMenuItem(MenuItemHook):  # pylint: disable=too-few-public-methods
         :param request:
         :return:
         """
+
         if request.user.has_perm("aasrp.basic_access"):
+            app_count = AaSrpManager.pending_requests_count_for_user(request.user)
+            self.count = app_count if app_count and app_count > 0 else None
+
             return MenuItemHook.render(self, request)
 
         return ""
@@ -43,6 +50,7 @@ def register_menu():
     register our menu item
     :return:
     """
+
     return AaSrpMenuItem()
 
 
@@ -52,4 +60,5 @@ def register_urls():
     register our basu url
     :return:
     """
+
     return UrlHook(urls, "aasrp", r"^ship-replacement/")
