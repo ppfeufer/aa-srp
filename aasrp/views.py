2c6c2ab7820828b0fd0483f8f0f43ad591141a9f
Peter Pfeufer <info@ppfeufer.de>
2020-12-01 13:03:01 +0100

    srp request added

--- aasrp/views.py
+++ aasrp/views.py
@@ -3,23 +3,26 @@
 """
 the views
 """
-from aasrp.form import AaSrpLinkForm, AaSrpLinkUpdateForm, AaSrpRequestForm
-from aasrp.models import AaSrpLink, AaSrpStatus, AaSrpRequest
-from aasrp.utils import LoggerAddTag
-
-from django.contrib.auth.decorators import login_required, permission_required
-
-# from django.db.models import Sum
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render, redirect
-from django.utils.crypto import get_random_string
-from django.utils.translation import gettext_lazy as _
 
 from aasrp import __title__
 from aasrp.app_settings import avoid_cdn
 from aasrp.view_helper import get_dashboard_action_buttons
+from aasrp.form import AaSrpLinkForm, AaSrpLinkUpdateForm, AaSrpRequestForm
+from aasrp.managers import AaSrpManager
+from aasrp.models import AaSrpLink, AaSrpStatus, AaSrpRequest
+from aasrp.utils import LoggerAddTag
+from allianceauth.eveonline.models import EveCharacter
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.utils.crypto import get_random_string
+from django.utils.translation import gettext_lazy as _
 
 from allianceauth.services.hooks import get_extension_logger
+from allianceauth.eveonline.providers import provider
 
 logger = LoggerAddTag(get_extension_logger(__name__), __title__)
 
@@ -31,7 +34,7 @@ def dashboard(request, show_all_links=False) -> HttpResponse:
     Index view
     """
 
-    logger.info("Module called by %s", request.user)
+    logger.info("Module called by {user}".format(user=request.user))
 
     context = {"avoid_cdn": avoid_cdn(), "show_all_links": show_all_links}
 
@@ -83,6 +86,8 @@ def active_srp_links_data(request, show_all_links=False) -> JsonResponse:
     return JsonResponse(data, safe=False)
 
 
+@login_required
+@permission_required("aasrp.basic_access")
 def user_srp_requests_data(request) -> JsonResponse:
     data = list()
 
@@ -92,8 +97,8 @@ def user_srp_requests_data(request) -> JsonResponse:
         killboard_link = ""
         if srp_request.killboard_link:
             killboard_link = (
-                '<a href="{zkb_link}" target="_blank">{zkb_link}</a>'.format(
-                    zkb_link=srp_request.killboard_link
+                '<a href="{zkb_link}" target="_blank">{zkb_link_text}</a>'.format(
+                    zkb_link=srp_request.killboard_link, zkb_link_text=_("Link")
                 )
             )
 
@@ -191,6 +196,8 @@ def srp_link_edit(request, srp_code: str) -> HttpResponse:
     return render(request, "aasrp/link_edit.html", context)
 
 
+@login_required
+@permission_required("aasrp.basic_access")
 def request_srp(request, srp_code: str) -> HttpResponse:
     """
     Request SRP view
@@ -198,39 +205,127 @@ def request_srp(request, srp_code: str) -> HttpResponse:
     :param srp_code:
     """
 
-    logger.info("SRP Request link called by %s", request.user)
+    logger.info(
+        "SRP Request for SRP code {srp_code} called by {user}".format(
+            user=request.user, srp_code=srp_code
+        )
+    )
+
+    # check if the provided SRP code is valid
+    if AaSrpLink.objects.filter(srp_code=srp_code).exists() is False:
+        logger.error(
+            "Unable to locate SRP Fleet using code {srp_code} for user {user}".format(
+                srp_code=srp_code, user=request.user
+            )
+        )
+
+        messages.error(
+            request,
+            _("Unable to locate SRP code with ID {srp_code}").format(srp_code=srp_code),
+        )
+        return redirect("aasrp:dashboard")
 
     # if this is a POST request we need to process the form data
     if request.method == "POST":
         # create a form instance and populate it with data from the request
         form = AaSrpRequestForm(request.POST)
 
+        logger.debug(
+            "Request type POST contains form valid: {form_is_valid}".format(
+                form_is_valid=form.is_valid()
+            )
+        )
+
         # check whether it's valid:
         if form.is_valid():
-            srp_name = form.cleaned_data["srp_name"]
-            fleet_time = form.cleaned_data["fleet_time"]
-            fleet_doctrine = form.cleaned_data["fleet_doctrine"]
-            aar_link = form.cleaned_data["aar_link"]
+            # check if the killmail was already posted
+            if AaSrpRequest.objects.filter(
+                killboard_link=form.cleaned_data["killboard_link"]
+            ).exists():
+                messages.error(
+                    request, _("There is already a SRP request for this killmail.")
+                )
+                return redirect("aasrp:dashboard")
 
-            srp_link = AaSrpLink()
-            srp_link.srp_name = srp_name
-            srp_link.fleet_time = fleet_time
-            srp_link.fleet_doctrine = fleet_doctrine
-            srp_link.aar_link = aar_link
-            srp_link.srp_code = get_random_string(length=16)
-            srp_link.fleet_commander = request.user.profile.main_character
-            srp_link.creator = request.user
-            srp_link.save()
+            creator = request.user
+            main_character = request.user.profile.main_character
+            srp_link = AaSrpLink.objects.get(srp_code=srp_code)
+            post_time = timezone.now()
 
-            request.session["msg"] = [
-                "success",
-                "SRP Link created",
-            ]
+            srp_request = AaSrpRequest()
+            srp_request.killboard_link = form.cleaned_data["killboard_link"]
+            srp_request.additional_info = form.cleaned_data["additional_info"]
+            srp_request.creator = creator
+            # srp_request.character = main_character
+            srp_request.srp_link = srp_link
+
+            # parse zkillboard killmail
+            try:
+                srp_kill_link = AaSrpManager.get_kill_id(srp_request.killboard_link)
+
+                (ship_type_id, ship_value, victim_id) = AaSrpManager.get_kill_data(
+                    srp_kill_link
+                )
+            except ValueError:
+                # invalid killmail
+                logger.debug(
+                    "User {user} submitted an invalid killmail link ({killmail_link}) "
+                    "or zKillboard server could not be reached".format(
+                        user=request.user, killmail_link=srp_request.killboard_link
+                    )
+                )
+
+                messages.error(
+                    request,
+                    _(
+                        "Your SRP request Killmail link is invalid. Please make sure you are using zKillboard."
+                    ),
+                )
+                return redirect("aasrp:dashboard")
+
+            if request.user.character_ownerships.filter(
+                character__character_id=str(victim_id)
+            ).exists():
+                character = EveCharacter.objects.get_character_by_id(victim_id)
+
+                # srp_request.character = request.user.character_ownerships.filter(
+                #     character__character_id=str(victim_id)
+                # )
+                srp_request.character = character
+                srp_request.ship_name = provider.get_itemtype(ship_type_id).name
+                srp_request.loss_amount = ship_value
+                srp_request.post_time = post_time
+                srp_request.request_code = get_random_string(length=16)
+                srp_request.save()
+
+                logger.info(
+                    "Created SRP Request on behalf of user %s for fleet name %s"
+                    % (request.user, srp_link.srp_name)
+                )
+                messages.success(
+                    request,
+                    _("Submitted SRP request for your {ship}.").format(
+                        ship=srp_request.ship_name
+                    ),
+                )
+                return redirect("aasrp:dashboard")
+            else:
+                messages.error(
+                    request,
+                    _(
+                        "Character {character_id} does not belong to your Auth account. "
+                        "Please add the API key for this character and try again"
+                    ).format(character_id=victim_id),
+                )
 
             return redirect("aasrp:dashboard")
 
     # if a GET (or any other method) we'll create a blank form
     else:
+        logger.debug(
+            "Returning blank SRP request form for {user}".format(user=request.user)
+        )
+
         form = AaSrpRequestForm()
 
     context = {"avoid_cdn": avoid_cdn(), "srp_code": srp_code, "form": form}
