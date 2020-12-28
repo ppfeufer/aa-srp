9cfde4a543d6ec77c53f2bcebcbb2e1eb26fff40
Peter Pfeufer <info@ppfeufer.de>
2020-12-28 14:22:43 +0100

    v0.1.0-beta.1

--- aasrp/__init__.py
+++ aasrp/__init__.py
@@ -6,7 +6,7 @@ a couple of variable to use throughout the app
 
 default_app_config: str = "aasrp.apps.AaSrpConfig"
 
-__version__ = "0.0.1-aplha.1"
+__version__ = "0.1.0-beta.1"
 __title__ = "Ship Replacement"
 __verbose_name__ = "AA-SRP - A ship replacement module for Alliance Auth"
 __user_agent__ = "{verbose_name} - v{version} - {github_url}".format(
