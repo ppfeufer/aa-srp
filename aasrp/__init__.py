630656a37e79bf084dd8b130a98c383ec94d32aa
Peter Pfeufer <info@ppfeufer.de>
2021-01-07 16:11:04 +0100

    apparently we can't call django modules here

--- aasrp/__init__.py
+++ aasrp/__init__.py
@@ -4,15 +4,14 @@
 a couple of variable to use throughout the app
 """
 
-from django.utils.text import slugify
-
 default_app_config: str = "aasrp.apps.AaSrpConfig"
 
 __version__ = "0.1.0-beta.9"
 __title__ = "Ship Replacement"
 __verbose_name__ = "AA-SRP - A ship replacement module for Alliance Auth"
+__user_agent__name__ = "AA-SRP-A-ship-replacement-module-for-Alliance-Auth"
 __user_agent__ = "{verbose_name} - v{version} - {github_url}".format(
-    verbose_name=slugify(__verbose_name__, allow_unicode=True),
+    verbose_name=__user_agent__name__,
     version=__version__,
     github_url="https://github.com/ppfeufer/aa-srp",
 )
