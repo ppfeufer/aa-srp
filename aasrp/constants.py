e33c840ad8ae7439b4d92306554898d63770f54e
Peter Pfeufer <info@ppfeufer.de>
2021-01-11 09:13:59 +0100

    useragent formatting

--- aasrp/constants.py
+++ aasrp/constants.py
@@ -9,7 +9,7 @@ from django.utils.text import slugify
 from aasrp import __version__
 
 VERBOSE_NAME = "AA-SRP - A ship replacement module for Alliance Auth"
-USERAGENT = "{verbose_name} - v{version} - {github_url}".format(
+USERAGENT = "{verbose_name} v{version} {github_url}".format(
     verbose_name=slugify(VERBOSE_NAME, allow_unicode=True),
     version=__version__,
     github_url="https://github.com/ppfeufer/aa-srp",
