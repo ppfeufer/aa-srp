43311b5c62ab77817a685ad608883dc88d307236
Peter Pfeufer <info@ppfeufer.de>
2021-02-04 23:15:25 +0100

    modal headers fixed

--- aasrp/__init__.py
+++ aasrp/__init__.py
@@ -6,5 +6,5 @@ a couple of variable to use throughout the app
 
 default_app_config: str = "aasrp.apps.AaSrpConfig"
 
-__version__ = "0.1.0-beta.14"
+__version__ = "0.1.0-beta.15"
 __title__ = "Ship Replacement"
