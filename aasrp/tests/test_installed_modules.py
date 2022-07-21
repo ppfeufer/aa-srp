d5b62dce4305cd53e4b6ea3cbd8fd687ef66cdc5
Peter Pfeufer <info@ppfeufer.de>
2022-07-21 11:45:41 +0200

    [FIX] Test for installed apps (hopefully)

--- aasrp/tests/test_installed_modules.py
+++ aasrp/tests/test_installed_modules.py
@@ -26,7 +26,9 @@ class TestModulesInstalled(TestCase):
 
         self.assertFalse(allianceauth_discordbot_installed())
 
-    @modify_settings(INSTALLED_APPS={"append": "aadiscordbot"})
+    @modify_settings(
+        INSTALLED_APPS={"append": "aadiscordbot"}, DISCORD_BOT_TOKEN="MYDUMMYTOKEN"
+    )
     def test_allianceauth_discordbot_installed_should_return_true(self):
         """
         Test allianceauth_discordbot_installed should return True
