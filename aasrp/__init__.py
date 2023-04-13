afd20e970d547c926816c7025e4c9689bad643ae
Peter Pfeufer <info@ppfeufer.de>
2023-04-13 22:55:10 +0200

    [Version bumped to] v1.17.1

--- aasrp/__init__.py
+++ aasrp/__init__.py
@@ -1,6 +1,6 @@
 """
-A couple of variable to use throughout the app
+A couple of variables to use throughout the app
 """
 
-__version__ = "1.17.0"
+__version__ = "1.17.1"
 __title__ = "Ship Replacement"
