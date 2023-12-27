79f5965f3e5ad0a45a3ab7d43dbc1ae4daf10c07
Peter Pfeufer <info@ppfeufer.de>
2023-12-27 18:29:47 +0100

    [CHANGE] Version moved to `__init__.py`

--- aasrp/__init__.py
+++ aasrp/__init__.py
@@ -2,13 +2,8 @@
 App init
 """
 
-# Standard Library
-from importlib import metadata
-
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = metadata.version(distribution_name="aa-srp")
+__version__ = "1.21.0"
 __title__ = _("Ship Replacement")
-
-del metadata
