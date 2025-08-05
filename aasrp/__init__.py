353175fc77f0aef9905750cc8917e587c39d5daa
Peter Pfeufer <info@ppfeufer.de>
2025-08-05 11:24:06 +0200

    [RELEASE] v2.11.0
    ## [2.11.0] - 2025-08-05
    
    ### Added
    
    - Bulk actions for approval and removal of SRP requests
    
    ### Changed
    
    - Switch from XHR requests to Fetch API for own ajax calls
    - Admin pages improved
    - Translations updated
    
    ### Fixed
    
    - SRP team Discord notification template to show additional information only when available

--- aasrp/__init__.py
+++ aasrp/__init__.py
@@ -5,7 +5,7 @@ App init
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "2.10.0"
+__version__ = "2.11.0"
 __title__ = _("Ship Replacement")
 
 __package_name__ = "aa-srp"
