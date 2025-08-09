0360b720150aacbc081751bc2c8603ce078d4d4e
Peter Pfeufer <info@ppfeufer.de>
2025-08-09 11:18:58 +0200

    [CHANGE] Use AA framework fetch API functions

--- aasrp/static/aasrp/javascript/my-srp-requests.js
+++ aasrp/static/aasrp/javascript/my-srp-requests.js
@@ -10,171 +10,171 @@ $(document).ready(() => {
      */
     let userSrpAmount = 0;
 
-    const tableUserSrpRequests = elementTableUserSrpRequests.DataTable({
-        language: aaSrpSettings.dataTable.language,
-        ajax: {
-            url: aaSrpSettings.url.userSrpRequests,
-            dataSrc: '',
-            cache: false
-        },
-        columns: [
-            {
-                data: 'request_time',
-                /**
-                 * Render callback
-                 *
-                 * @param data
-                 * @returns {*}
-                 */
-                render: {
-                    display: (data) => {
-                        return data === null ? '' : moment(data).utc().format(
-                            aaSrpSettings.datetimeFormat
-                        );
+    fetchGet({url: aaSrpSettings.url.userSrpRequests})
+        .then((data) => {
+            if (data) {
+                elementTableUserSrpRequests.DataTable({
+                    language: aaSrpSettings.dataTable.language,
+                    data: data,
+                    columns: [
+                        {
+                            data: 'request_time',
+                            /**
+                             * Render callback
+                             *
+                             * @param data
+                             * @returns {*}
+                             */
+                            render: {
+                                display: (data) => {
+                                    return data === null ? '' : moment(data).utc().format(
+                                        aaSrpSettings.datetimeFormat
+                                    );
+                                },
+                                sort: (data) => {
+                                    return data === null ? '' : data;
+                                }
+                            },
+                            className: 'srp-request-time'
+                        },
+                        {
+                            data: 'character_html',
+                            render: {
+                                display: 'display',
+                                filter: 'sort',
+                                sort: 'sort'
+                            },
+                            className: 'srp-request-character'
+                        },
+                        {
+                            data: 'fleet_name_html',
+                            /**
+                             * Render callback
+                             */
+                            render: {
+                                display: 'display',
+                                filter: 'sort',
+                                sort: 'sort'
+                            },
+                            className: 'srp-request-fleet-details'
+                        },
+                        // {
+                        //     data: 'srp_code',
+                        //     className: 'srp-request-srp-code'
+                        // },
+                        // {
+                        //     data: 'request_code',
+                        //     className: 'srp-request-code'
+                        // },
+                        {
+                            data: 'ship_html',
+                            render: {
+                                display: 'display',
+                                filter: 'sort',
+                                sort: 'sort'
+                            },
+                            className: 'srp-request-ship'
+                        },
+                        // {data: 'zkb_link'},
+                        {
+                            data: 'zkb_loss_amount_html',
+                            /**
+                             * Render callback
+                             */
+                            render: {
+                                display: 'display',
+                                filter: 'sort',
+                                sort: 'sort'
+                            },
+                            className: 'srp-request-zkb-loss-amount text-end'
+                        },
+                        {
+                            data: 'zkb_loss_amount_html',
+                            /**
+                             * Render callback
+                             */
+                            render: {
+                                display: 'display',
+                                filter: 'sort',
+                                sort: 'sort'
+                            },
+                            className: 'srp-request-payout text-end'
+                        },
+                        {
+                            data: 'request_status_icon',
+                            className: 'srp-request-status text-end'
+                        },
+                        // Hidden columns
+                        {data: 'request_status'},
+                        {data: 'ship'},
+                        {data: 'character'}
+                    ],
+                    columnDefs: [
+                        {
+                            orderable: false,
+                            targets: [6]
+                        },
+                        {
+                            visible: false,
+                            targets: [7, 8, 9]
+                        }
+                    ],
+                    order: [
+                        [0, 'desc']
+                    ],
+                    filterDropDown: {
+                        columns: [
+                            {
+                                idx: 9,
+                                title: aaSrpSettings.translation.filter.character
+                            },
+                            {
+                                idx: 8,
+                                title: aaSrpSettings.translation.filter.ship
+                            },
+                            {
+                                idx: 7,
+                                title: aaSrpSettings.translation.filter.requestStatus
+                            }
+                        ],
+                        autoSize: false,
+                        bootstrap: true,
+                        bootstrap_version: 5
                     },
-                    sort: (data) => {
-                        return data === null ? '' : data;
+                    /**
+                     * When ever a row is created …
+                     *
+                     * @param row
+                     * @param data
+                     * @param rowIndex
+                     */
+                    createdRow: (row, data, rowIndex) => {
+                        // Row id attr
+                        $(row).attr('data-row-id', rowIndex);
+                        $(row).attr('data-srp-request-code', data.request_code);
+
+                        userSrpAmount += parseInt(data.payout_amount);
+
+                        $('.srp-dashboard-user-isk-cost-amount').html(
+                            `${new Intl.NumberFormat(aaSrpSettings.locale).format(userSrpAmount)} ISK`
+                        );
                     }
-                },
-                className: 'srp-request-time'
-            },
-            {
-                data: 'character_html',
-                render: {
-                    display: 'display',
-                    filter: 'sort',
-                    sort: 'sort'
-                },
-                className: 'srp-request-character'
-            },
-            {
-                data: 'fleet_name_html',
-                /**
-                 * Render callback
-                 */
-                render: {
-                    display: 'display',
-                    filter: 'sort',
-                    sort: 'sort'
-                },
-                className: 'srp-request-fleet-details'
-            },
-            // {
-            //     data: 'srp_code',
-            //     className: 'srp-request-srp-code'
-            // },
-            // {
-            //     data: 'request_code',
-            //     className: 'srp-request-code'
-            // },
-            {
-                data: 'ship_html',
-                render: {
-                    display: 'display',
-                    filter: 'sort',
-                    sort: 'sort'
-                },
-                className: 'srp-request-ship'
-            },
-            // {data: 'zkb_link'},
-            {
-                data: 'zkb_loss_amount_html',
-                /**
-                 * Render callback
-                 */
-                render: {
-                    display: 'display',
-                    filter: 'sort',
-                    sort: 'sort'
-                },
-                className: 'srp-request-zkb-loss-amount text-end'
-            },
-            {
-                data: 'zkb_loss_amount_html',
-                /**
-                 * Render callback
-                 */
-                render: {
-                    display: 'display',
-                    filter: 'sort',
-                    sort: 'sort'
-                },
-                className: 'srp-request-payout text-end'
-            },
-            {
-                data: 'request_status_icon',
-                className: 'srp-request-status text-end'
-            },
-            // Hidden columns
-            {data: 'request_status'},
-            {data: 'ship'},
-            {data: 'character'}
-        ],
-        columnDefs: [
-            {
-                orderable: false,
-                targets: [6]
-            },
-            {
-                visible: false,
-                targets: [7, 8, 9]
+                });
             }
-        ],
-        order: [
-            [0, 'desc']
-        ],
-        filterDropDown: {
-            columns: [
-                {
-                    idx: 9,
-                    title: aaSrpSettings.translation.filter.character
-                },
-                {
-                    idx: 8,
-                    title: aaSrpSettings.translation.filter.ship
-                },
-                {
-                    idx: 7,
-                    title: aaSrpSettings.translation.filter.requestStatus
-                }
-            ],
-            autoSize: false,
-            bootstrap: true,
-            bootstrap_version: 5
-        },
-        /**
-         * When ever a row is created …
-         *
-         * @param row
-         * @param data
-         * @param rowIndex
-         */
-        createdRow: (row, data, rowIndex) => {
-            // Row id attr
-            $(row).attr('data-row-id', rowIndex);
-            $(row).attr('data-srp-request-code', data.request_code);
-
-            userSrpAmount += parseInt(data.payout_amount);
-
-            $('.srp-dashboard-user-isk-cost-amount').html(
-                `${new Intl.NumberFormat(aaSrpSettings.locale).format(userSrpAmount)} ISK`
-            );
-        }
-    });
-
-    /**
-     * When the DataTable has finished rendering and is fully initialized
-     */
-    tableUserSrpRequests.on('draw', () => {
-        // Show bootstrap tooltips
-        [].slice.call(
-            document.querySelectorAll(
-                '[data-bs-tooltip="aa-srp"]'
-            )
-        ).map((tooltipTriggerEl) => {
-            return new bootstrap.Tooltip(tooltipTriggerEl);
+        })
+        .then(() => { // When the DataTable has finished rendering and is fully initialized
+            // Show bootstrap tooltips
+            [].slice.call(
+                document.querySelectorAll(
+                    '[data-bs-tooltip="aa-srp"]'
+                )
+            ).map((tooltipTriggerEl) => {
+                return new bootstrap.Tooltip(tooltipTriggerEl);
+            });
+        })
+        .catch((error) => {
+            console.error('Error fetching SRP request data:', error);
         });
-    });
 
     /**
      * Modals
@@ -189,7 +189,8 @@ $(document).ready(() => {
         fetchGet({url: url, responseIsJson: false})
             .then((data) => {
                 modalSrpRequestDetails.find('.modal-body').html(data);
-            }).catch((error) => {
+            })
+            .catch((error) => {
                 console.log(`Error: ${error.message}`);
             });
     }).on('hide.bs.modal', () => {
--- aasrp/static/aasrp/javascript/srp-links.js
+++ aasrp/static/aasrp/javascript/srp-links.js
@@ -1,4 +1,4 @@
-/* global moment, aaSrpSettings, bootstrap */
+/* global moment, aaSrpSettings, bootstrap, fetchGet */
 
 $(document).ready(() => {
     'use strict';
@@ -10,145 +10,145 @@ $(document).ready(() => {
      */
     let totalSrpAmount = 0;
 
-    const tableSrpLinks = elementTableSrpLinks.DataTable({
-        language: aaSrpSettings.dataTable.language,
-        ajax: {
-            url: aaSrpSettings.url.availableSrpLinks,
-            dataSrc: '',
-            cache: false
-        },
-        columns: [
-            {
-                data: 'srp_name',
-                className: 'srp-link-fleet-name'
-            },
-            {
-                data: 'creator',
-                className: 'srp-link-creator'
-            },
-            {
-                data: 'fleet_time',
-                /**
-                 * Render callback
-                 */
-                render: {
+    fetchGet({url: aaSrpSettings.url.availableSrpLinks})
+        .then((data) => {
+            if (data) {
+                elementTableSrpLinks.DataTable({
+                    language: aaSrpSettings.dataTable.language,
+                    data: data,
+                    columns: [
+                        {
+                            data: 'srp_name',
+                            className: 'srp-link-fleet-name'
+                        },
+                        {
+                            data: 'creator',
+                            className: 'srp-link-creator'
+                        },
+                        {
+                            data: 'fleet_time',
+                            /**
+                             * Render callback
+                             */
+                            render: {
+                                /**
+                                 * Display callback
+                                 *
+                                 * @param {string} data
+                                 * @returns {string|*}
+                                 */
+                                display: (data) => {
+                                    return data === null ? '' : moment(data).utc().format(
+                                        aaSrpSettings.datetimeFormat
+                                    );
+                                },
+                                /**
+                                 * Sort callback
+                                 *
+                                 * @param {string} data
+                                 * @returns {string|*}
+                                 */
+                                sort: (data) => {
+                                    return data === null ? '' : data;
+                                }
+                            },
+                            className: 'srp-link-fleet-time'
+                        },
+                        {
+                            data: 'fleet_type',
+                            className: 'srp-link-fleet-type'
+                        },
+                        // {
+                        //     data: 'fleet_commander',
+                        //     className: 'srp-link-fleet-commander'
+                        // },
+                        {
+                            data: 'fleet_doctrine',
+                            className: 'srp-link-fleet-doctrine'
+                        },
+                        {
+                            data: 'aar_link',
+                            className: 'srp-link-aar-link'
+                        },
+                        {
+                            data: 'srp_code',
+                            render: {
+                                display: 'display',
+                                filter: 'sort',
+                                sort: 'sort'
+                            },
+                            className: 'srp-link-code'
+                        },
+                        {
+                            data: 'srp_costs_html',
+                            /**
+                             * Render callback
+                             */
+                            render: {
+                                display: 'display',
+                                filter: 'sort',
+                                sort: 'sort'
+                            },
+                            className: 'srp-link-total-cost text-end'
+                        },
+                        {
+                            data: 'srp_status',
+                            className: 'srp-link-status'
+                        },
+                        {
+                            data: 'pending_requests',
+                            className: 'srp-link-pending-requests text-end'
+                        },
+                        {
+                            data: 'actions',
+                            className: 'srp-link-actions text-end'
+                        }
+                    ],
+                    columnDefs: [
+                        {
+                            orderable: false,
+                            targets: [10]
+                        },
+                        {
+                            width: 115,
+                            targets: [10]
+                        }
+                    ],
+                    order: [[2, 'asc']],
+                    paging: aaSrpSettings.dataTable.paging,
                     /**
-                     * Display callback
+                     * When ever a row is created …
                      *
-                     * @param {string} data
-                     * @returns {string|*}
+                     * @param row
+                     * @param data
+                     * @param rowIndex
                      */
-                    display: (data) => {
-                        return data === null ? '' : moment(data).utc().format(
-                            aaSrpSettings.datetimeFormat
+                    createdRow: (row, data, rowIndex) => {
+                        // Row id attr
+                        $(row)
+                            .attr('data-row-id', rowIndex)
+                            .attr('data-srp-code', data.srp_code.sort);
+
+                        totalSrpAmount += parseInt(data.srp_costs);
+
+                        $('.srp-dashboard-total-isk-cost-amount').html(
+                            `${new Intl.NumberFormat(aaSrpSettings.locale).format(totalSrpAmount)} ISK`
                         );
-                    },
-                    /**
-                     * Sort callback
-                     *
-                     * @param {string} data
-                     * @returns {string|*}
-                     */
-                    sort: (data) => {
-                        return data === null ? '' : data;
                     }
-                },
-                className: 'srp-link-fleet-time'
-            },
-            {
-                data: 'fleet_type',
-                className: 'srp-link-fleet-type'
-            },
-            // {
-            //     data: 'fleet_commander',
-            //     className: 'srp-link-fleet-commander'
-            // },
-            {
-                data: 'fleet_doctrine',
-                className: 'srp-link-fleet-doctrine'
-            },
-            {
-                data: 'aar_link',
-                className: 'srp-link-aar-link'
-            },
-            {
-                data: 'srp_code',
-                render: {
-                    display: 'display',
-                    filter: 'sort',
-                    sort: 'sort'
-                },
-                className: 'srp-link-code'
-            },
-            {
-                data: 'srp_costs_html',
-                /**
-                 * Render callback
-                 */
-                render: {
-                    display: 'display',
-                    filter: 'sort',
-                    sort: 'sort'
-                },
-                className: 'srp-link-total-cost text-end'
-            },
-            {
-                data: 'srp_status',
-                className: 'srp-link-status'
-            },
-            {
-                data: 'pending_requests',
-                className: 'srp-link-pending-requests text-end'
-            },
-            {
-                data: 'actions',
-                className: 'srp-link-actions text-end'
+                });
             }
-        ],
-        columnDefs: [
-            {
-                orderable: false,
-                targets: [10]
-            },
-            {
-                width: 115,
-                targets: [10]
-            }
-        ],
-        order: [[2, 'asc']],
-        paging: aaSrpSettings.dataTable.paging,
-        /**
-         * When ever a row is created …
-         *
-         * @param row
-         * @param data
-         * @param rowIndex
-         */
-        createdRow: (row, data, rowIndex) => {
-            // Row id attr
-            $(row)
-                .attr('data-row-id', rowIndex)
-                .attr('data-srp-code', data.srp_code.sort);
-
-            totalSrpAmount += parseInt(data.srp_costs);
-
-            $('.srp-dashboard-total-isk-cost-amount').html(
-                `${new Intl.NumberFormat(aaSrpSettings.locale).format(totalSrpAmount)} ISK`
-            );
-        }
-    });
-
-    /**
-     * When the DataTable has finished rendering and is fully initialized
-     */
-    tableSrpLinks.on('draw', () => {
-        // Show bootstrap tooltips
-        [].slice.call(
-            document.querySelectorAll('[data-bs-tooltip="aa-srp"]')
-        ).map((tooltipTriggerEl) => {
-            return new bootstrap.Tooltip(tooltipTriggerEl);
+        })
+        .then(() => { // When the DataTable has finished rendering and is fully initialized
+            // Show bootstrap tooltips
+            [].slice.call(
+                document.querySelectorAll('[data-bs-tooltip="aa-srp"]')
+            ).map((tooltipTriggerEl) => {
+                return new bootstrap.Tooltip(tooltipTriggerEl);
+            });
+        })
+        .catch((error) => {
+            console.error('Error fetching SRP links:', error);
         });
-    });
 
     /**
      * Modals
@@ -165,9 +165,7 @@ $(document).ready(() => {
 
         modalEnableSrpLink.find('#modal-button-confirm-enable-srp-link')
             .attr('href', url);
-        modalEnableSrpLink.find('.modal-body').html(
-            `${aaSrpSettings.translation.modal.enableSrpLink.body}<p class="fw-bold">${name}</p>`
-        );
+        modalEnableSrpLink.find('.modal-body').html(`${aaSrpSettings.translation.modal.enableSrpLink.body}<p class="fw-bold">${name}</p>`);
     }).on('hide.bs.modal', () => {
         modalEnableSrpLink.find('.modal-body').html('');
     });
@@ -180,9 +178,7 @@ $(document).ready(() => {
 
         modalDisableSrpLink.find('#modal-button-confirm-disable-srp-link')
             .attr('href', url);
-        modalDisableSrpLink.find('.modal-body').html(
-            `${aaSrpSettings.translation.modal.disableSrpLink.body}<p class="fw-bold">${name}</p>`
-        );
+        modalDisableSrpLink.find('.modal-body').html(`${aaSrpSettings.translation.modal.disableSrpLink.body}<p class="fw-bold">${name}</p>`);
     }).on('hide.bs.modal', () => {
         modalDisableSrpLink.find('.modal-body').html('');
     });
@@ -193,11 +189,8 @@ $(document).ready(() => {
         const url = button.data('url');
         const name = button.data('name');
 
-        modalDeleteSrpLink.find('#modal-button-confirm-delete-srp-link')
-            .attr('href', url);
-        modalDeleteSrpLink.find('.modal-body').html(
-            `${aaSrpSettings.translation.modal.deleteSrpLink.body}<p class="fw-bold">${name}</p>`
-        );
+        modalDeleteSrpLink.find('#modal-button-confirm-delete-srp-link').attr('href', url);
+        modalDeleteSrpLink.find('.modal-body').html(`${aaSrpSettings.translation.modal.deleteSrpLink.body}<p class="fw-bold">${name}</p>`);
     }).on('hide.bs.modal', () => {
         modalDeleteSrpLink.find('.modal-body').html('');
     });
