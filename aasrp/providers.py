4a9e9a08b56bc8aa8086e7c5ee7b2f327118261a
Peter Pfeufer <info@ppfeufer.de>
2025-08-27 15:03:06 +0200

    [CHANGE] Switch to OpenAPI

--- aasrp/providers.py
+++ aasrp/providers.py
@@ -3,12 +3,22 @@ Providers
 """
 
 # Alliance Auth
-from esi.clients import EsiClientProvider
+from esi.openapi_clients import ESIClientProvider
 
 # AA SRP
-from aasrp import __app_name_useragent__, __github_url__, __version__
+from aasrp import (
+    __app_name_useragent__,
+    __esi_compatibility_date__,
+    __github_url__,
+    __version__,
+)
 
 # ESI client
-esi = EsiClientProvider(
-    ua_appname=__app_name_useragent__, ua_version=__version__, ua_url=__github_url__
+esi = ESIClientProvider(
+    # Use the latest compatibility date, see https://esi.evetech.net/meta/compatibility-dates
+    compatibility_date=__esi_compatibility_date__,
+    # User agent for the ESI client
+    ua_appname=__app_name_useragent__,
+    ua_version=__version__,
+    ua_url=__github_url__,
 )
