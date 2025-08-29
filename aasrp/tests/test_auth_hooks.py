da7a899111d1af5d36274350f38c16ae72d49582
Peter Pfeufer <info@ppfeufer.de>
2025-08-29 23:37:31 +0200

    [CHANGE] Bit of code cleanup

--- aasrp/tests/test_auth_hooks.py
+++ aasrp/tests/test_auth_hooks.py
@@ -112,7 +112,7 @@ class TestAaSrpMenuItem(TestCase):
 
         self.assertEqual(result, "")
 
-    @patch("aasrp.auth_hooks.SrpRequest.objects.pending_requests_count_for_user")
+    @patch("aasrp.auth_hooks.SrpRequest.pending_requests_count_for_user")
     def test_render_with_permission(self, mock_pending_requests):
         """
         Test should return html if a user has permission
