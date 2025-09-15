aa52704d43e00174c9535edd83d43566865cc601
Peter Pfeufer <info@ppfeufer.de>
2025-09-15 12:51:56 +0200

    [FIX] App title

--- aasrp/__init__.py
+++ aasrp/__init__.py
@@ -1,12 +1,13 @@
 """
-App init
+App initialization module for the AA-SRP application.
+This module defines metadata and constants for the Ship Replacement Program (SRP) app.
 """
 
 # Django
 from django.utils.translation import gettext_lazy as _
 
 __version__ = "2.13.0"
-__title__ = _("Ship Replacement")
+__title__ = "Ship Replacement"
 __title_translated__ = _("Ship Replacement")
 __verbose_name__ = "Ship Replacement (SRP) for Alliance Auth"
 
@@ -15,6 +16,6 @@ __esi_compatibility_date__ = "2025-08-26"
 __package_name__ = "aa-srp"
 __app_name__ = "aasrp"
 __app_name_verbose__ = "AA SRP"
-__app_name_useragent__ = "AA-SRP"
+__app_name_useragent__ = "AaSrp"
 
 __github_url__ = f"https://github.com/ppfeufer/{__package_name__}"
